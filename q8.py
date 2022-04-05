c7a2899255640ce31a3ecae8c549202ec8c06534
Barış Aytekin <barisaytekin5@gmail.com>
2022-04-05 12:15:33 +0300

    Delete q8.py

--- q8.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("./drive/My Drive/urls - Sheet1.csv")
-for i in range(df.shape[0]):
-  start = df.iloc[i,1].find("/")
-  end = df.iloc[i,1].rfind("<")
-  print(start,end)
-  df.iloc[i,1] =df.iloc[i,1][start+2:end]
-df
